e71cb7ba472fa14a3d1a76e2a70a3161d66ee464
Hikari Sakamoto <hikari@townsq.com.br>
2020-10-02 18:35:07 -0300

    Basic data model, Records and Discriminated Unions

--- Model.fs
+++ Model.fs
@@ -1,2 +1,40 @@
 module fsharpening.Model
 
+
+// Record Type
+// with Structural Equality
+type Details = { Name: string; Description: string }
+
+type Item = { Details: Details }
+
+// Discriminated Unions
+// * is a tuple
+type Exit =
+    | PassableExit of Details * destination: Room
+    | LockedExit of Details * key: Item * next: Exit
+    | NoExit of Details option // Optional Type
+
+// Recursive Type Definition (and)
+and Exits =
+    { North: Exit
+      South: Exit
+      East: Exit
+      West: Exit }
+
+and Room =
+    { Details: Details
+      Items: Item list
+      Exits: Exits }
+
+
+// Defining a room
+let firstRoom =
+    { Details =
+          { Name = "First Room"
+            Description = "You're standing in a room" }
+      Items = []
+      Exits =
+          { North = NoExit(None)
+            South = NoExit(None)
+            East = NoExit(None)
+            West = NoExit(None) } }
