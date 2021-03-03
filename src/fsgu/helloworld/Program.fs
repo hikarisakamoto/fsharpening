5f6d1bfd3bb6343d20e61daebacfa80fabc7465f
Hikari Sakamoto <hikari@townsq.com.br>
2021-03-03 14:15:09 -0300

    With temp values

--- src/fsgu/helloworld/Program.fs
+++ src/fsgu/helloworld/Program.fs
@@ -3,9 +3,14 @@
 let sayHello person =
     printfn "Hello %s from my F# program" person
 
+let isValid person = not (String.IsNullOrWhiteSpace person)
+
 [<EntryPoint>]
 let main argv =
-    Array.iter sayHello argv   
+    argv
+    |> Array.filter isValid
+    |> Array.iter sayHello
+
     printfn "Nice to meet you."
 
     0 // return an integer exit code
