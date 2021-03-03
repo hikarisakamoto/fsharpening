80c2322d331d0c5db5708cf30ad0876b1c992169
Hikari Sakamoto <hikari@townsq.com.br>
2021-03-03 16:35:50 -0300

    print mean score

--- src/fsgu/StudentsScores/Program.fs
+++ src/fsgu/StudentsScores/Program.fs
@@ -1,15 +1,26 @@
 ﻿open System
+open System.IO
+
+let printMeanScore (row: string) =
+    let elements = row.Split('\t')
+    printfn "%s" row
+
+let summarize filePath =
+    let rows = File.ReadAllLines filePath
+    let studentCount = (rows |> Array.length) - 1
+    printfn "Student count %i" studentCount
+    rows
+    |> Array.skip 1
+    |> Array.iter printMeanScore
 
 [<EntryPoint>]
 let main argv =
     if argv.Length = 1 then
         let filePath = argv.[0]
 
-        if System.IO.File.Exists filePath then
+        if File.Exists filePath then
             printfn "Processing %s" filePath
-            let rows = System.IO.File.ReadAllLines filePath
-            let studentCount = (rows |> Array.length) - 1
-            printfn "Student count %i" studentCount
+            summarize filePath
             0
         else
             printfn "File not found: %s" filePath
