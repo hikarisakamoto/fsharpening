429faae64c104e0d2519dbe666ef25130f1f20b8
Hikari Sakamoto <hikari@townsq.com.br>
2021-03-03 17:08:13 -0300

    Record types

--- src/fsgu/StudentsScores/Program.fs
+++ src/fsgu/StudentsScores/Program.fs
@@ -1,26 +1,58 @@
 ﻿open System
 open System.IO
 
-let printMeanScore (row: string) =
-    let elements = row.Split('\t')
-    let name = elements.[0]
-    let id = elements.[1]
-    let scores = 
-        elements
-        |> Array.skip 2
-        |> Array.map float
-    let meanScore = scores |> Array.average
-    let minScore = scores |> Array.min
-    let maxScore = scores |> Array.max
-    printfn "%s\t%s\t%0.1f\t%0.1f\t%0.1f" name id meanScore minScore maxScore
+type Student =
+    {
+        Id: string
+        Name: string
+        MeanScore: float
+        MinScore: float
+        MaxScore: float
+    }
+
+module Student =
+    let fromString (s: string) =
+        let elements = s.Split('\t')
+        let name = elements.[0]
+        let id = elements.[1]
+
+        let scores =
+            elements |> Array.skip 2 |> Array.map float
+
+        let meanScore = scores |> Array.average
+        let minScore = scores |> Array.min
+        let maxScore = scores |> Array.max
+
+        {
+            Id = id
+            Name = name
+            MeanScore = meanScore
+            MinScore = minScore
+            MaxScore = maxScore
+        }
+
+    let printSummary (student: Student) =
+        printfn
+            "%s\t%s\t%0.1f\t%0.1f\t%0.1f"
+            student.Name
+            student.Id
+            student.MeanScore
+            student.MinScore
+            student.MaxScore
+
+let getSortKey (student: Student) = student.MeanScore
 
 let summarize filePath =
     let rows = File.ReadAllLines filePath
     let studentCount = (rows |> Array.length) - 1
+
     printfn "Student count %i" studentCount
+
     rows
     |> Array.skip 1
-    |> Array.iter printMeanScore
+    |> Array.map Student.fromString
+    |> Array.sortBy (fun student -> student.Name)
+    |> Array.iter Student.printSummary
 
 [<EntryPoint>]
 let main argv =
