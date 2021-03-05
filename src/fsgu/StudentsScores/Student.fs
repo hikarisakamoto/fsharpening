476b413d44b0cc641297ee1f13d2c7931ebeb1d9
Hikari Sakamoto <hikari@townsq.com.br>
2021-03-05 16:58:20 -0300

    Pattern matching

--- src/fsgu/StudentsScores/Student.fs
+++ src/fsgu/StudentsScores/Student.fs
@@ -1,36 +1,56 @@
-namespace StudentsScores
+namespace StudentScores
+
+open StudentsScore
 
 type Student =
     {
+        Surname: string
+        GivenName: string
         Id: string
-        Name: string
         MeanScore: float
         MinScore: float
         MaxScore: float
     }
 
 module Student =
+
+    let nameParts (s: string) =
+        let elements = s.Split(',')
+        let surname = elements.[0].Trim()
+        let givenName = elements.[1].Trim()
+        surname, givenName
+
     let fromString (s: string) =
         let elements = s.Split('\t')
         let name = elements.[0]
+        let surname, givenName = name |> nameParts
         let id = elements.[1]
 
         let scores =
             elements
             |> Array.skip 2
-            |> Array.choose Float.tryFromString
+            |> Array.map TestResult.fromString
+            |> Array.choose TestResult.tryEffectiveScore
 
         let meanScore = scores |> Array.average
         let minScore = scores |> Array.min
         let maxScore = scores |> Array.max
 
         {
+            Surname = surname
+            GivenName = givenName
             Id = id
-            Name = name
             MeanScore = meanScore
             MinScore = minScore
             MaxScore = maxScore
         }
 
     let printSummary (student: Student) =
-        printfn "%s	%s	%0.1f	%0.1f	%0.1f" student.Name student.Id student.MeanScore student.MinScore student.MaxScore
+        printfn
+            "%s, %s\t%s\t%0.1f\t%0.1f\t%0.1f"
+            student.Surname
+            student.GivenName
+            student.Id
+            student.MeanScore
+            student.MinScore
+            student.MaxScore
--- src/fsgu/StudentsScores/TestResult.fs
+++ src/fsgu/StudentsScores/TestResult.fs
@@ -2,15 +2,26 @@ namespace StudentsScore
 
 type TestResult =
     | Absent
-    | Excuse
+    | Excused
+    | Voided
     | Scored of float
 
 module TestResult =
+
     let fromString str =
         if str = "A" then
             Absent
         elif str = "E" then
-            Excuse
+            Excused
+        elif str = "V" then
+            Voided
         else
             let value = str |> float
             Scored value
+
+    let tryEffectiveScore (testResult: TestResult) =
+        match testResult with
+        | Absent -> Some 0.0
+        | Excused
+        | Voided -> None
+        | Scored score -> Some score
